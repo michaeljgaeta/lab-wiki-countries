f6c3a57d67b819387e847e61a3f8f7cfb8c4ca22
michaeljgaeta <michaeljgaeta@gmail.com>
2020-06-01 17:13:42 +0100

    boilerplate iteration 1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
